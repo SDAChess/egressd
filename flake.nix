487fc6dde91329e30027b415d8e2f777935f7105
Simon Scatton <simon.scatton@outlook.fr>
2026-05-22 20:08:34 +0200

    chore: initialize rust project

--- flake.nix
+++ flake.nix
@@ -3,19 +3,24 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     treefmt-nix = {
       url = "github:numtide/treefmt-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
     {
       self,
       nixpkgs,
-      treefmt-nix,
       flake-utils,
+      rust-overlay,
+      treefmt-nix,
     }:
     flake-utils.lib.eachSystem
       [
@@ -25,7 +30,19 @@
       (
         system:
         let
-          pkgs = nixpkgs.legacyPackages.${system};
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ rust-overlay.overlays.default ];
+          };
+
+          rustToolchain = pkgs.rust-bin.stable."1.95.0".default.override {
+            extensions = [
+              "rust-src"
+              "rust-analyzer"
+              "clippy"
+              "rustfmt"
+            ];
+          };
 
           treefmt = treefmt-nix.lib.evalModule pkgs {
             projectRootFile = "flake.nix";
@@ -34,6 +51,10 @@
           };
         in
         {
+          devShells.default = pkgs.mkShell {
+            packages = [ rustToolchain ];
+          };
+
           formatter = treefmt.config.build.wrapper;
 
           checks.formatting = treefmt.config.build.check self;
